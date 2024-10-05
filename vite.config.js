8fb443ed45c15292543aac4af8debb0e626a64db
afifaarg <argoubafifa1@gmail.com>
2024-10-05 21:56:49 +0100

    first commit2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173, // Use PORT from environment or default to 5173
+    host: true, // Expose the app to external requests (0.0.0.0)
+  },
+});
